ec8a32bd1bf1e5a65946ddef6bb8cdbad177c51a
CeicaIFCD0112 <rod.balseiro.ricardo@gmail.com>
2024-06-07 18:27:31 +0200

    1st

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'app/mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0730b132b8be8eb60649460b61b1c17709c9cad4',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'app/mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0730b132b8be8eb60649460b61b1c17709c9cad4',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
